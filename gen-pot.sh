b0248ac213e2a231da7633c7a03ccada1a920d86
David Olivier <machaven@users.sourceforge.net>
2007-03-29 21:38:48 +0000

    This script generate the .pot file from the sources list, using xgettext. This pot file will be used for new translation catalogs, and updating po files.
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/server-scripts@2646 b72ed6b6-b9f8-46b5-92b4-906544132732

---
+++ gen-pot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#This file generates the .pot file template, to be used for new translations, and merging with existing .po files
+#Change potfiles.txt location
+echo Generating POT File;
+xgettext --language=php --keyword=gT --from-code=UTF-8 --files-from=potfiles.txt --output=phpsurveyor.pot
+echo updating CHARSET to UTF-8;
+cp phpsurveyor.pot phpsurveyor.bak;
+cat phpsurveyor.bak | sed "s/charset=CHARSET/charset=UTF-8/" > phpsurveyor.pot
+echo updating Language-Team;
+cp phpsurveyor.pot phpsurveyor.bak;
+cat phpsurveyor.bak | sed "s/Language-Team: LANGUAGE <LL@li.org>/Language-Team: PHPSurveyor <c_schmitz@users.sourceforge.net>/" > phpsurveyor.pot
+echo updating PACKAGE VERSION;
+cp phpsurveyor.pot phpsurveyor.bak;
+cat phpsurveyor.bak | sed "s/PACKAGE VERSION/PHPSurveyor language file/" > phpsurveyor.pot
+echo updating HEADER;
+cp phpsurveyor.pot phpsurveyor.bak;
+cat phpsurveyor.bak | sed "s/SOME DESCRIPTIVE TITLE/PHPSURVEYOR LANGUAGE FILE/" > phpsurveyor.pot
+cp phpsurveyor.pot phpsurveyor.bak;
+cat phpsurveyor.bak | sed "s/Copyright (C) YEAR THE PACKAGE'S COPYRIGHT HOLDER/Copyright (C) 2007 PHPSurveyor Team/" > phpsurveyor.pot
+cp phpsurveyor.pot phpsurveyor.bak;
+cat phpsurveyor.bak | sed "s/This file is distributed under the same license as the PACKAGE package/This file is distributed under the same license as the PHPSurveyor package/" > phpsurveyor.pot
+cp phpsurveyor.pot phpsurveyor.bak;
+cat phpsurveyor.bak | sed "s/FIRST AUTHOR <EMAIL@ADDRESS>, YEAR/FIRST AUTHOR c_schmitz@users.sourceforge.net, 2007/" > phpsurveyor.pot
+echo DONE
